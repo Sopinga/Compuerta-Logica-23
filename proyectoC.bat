1335f0fd1f2b95ccd629037a7c0bf8fd1e19c3d8
Nicolás Gómez <Vlacyadou2017@gmail.com>
2022-10-11 15:55:40 -0300

    comiteado

---
+++ proyectoC.bat
@@ -0,0 +1,24 @@
+mkdir proyecto
+cd proyecto
+mkdir src
+mkdir docs
+mkdir test
+cd src
+dotnet new console -n Program
+dotnet new classlib -n Library
+cd Program
+dotnet add Program.csproj reference ../Library/Library.csproj
+cd ..
+cd ..
+cd test
+dotnet new nunit -n LibraryTests
+cd LibraryTests
+dotnet add LibraryTests.csproj reference ../../src/Library/Library.csproj
+cd ..
+cd ..
+dotnet new sln
+dotnet sln add src/Library/Library.csproj
+dotnet sln add src/Program/Program.csproj
+dotnet sln add test/LibraryTests/LibraryTests.csproj
+dotnet new gitignore
+code .
